5c532fdb0292e97ba2b1da696905669d557c5dd9
vinay <vinee9708@gmail.com>
2026-09-10 09:48:30 +0530

    Problem 3

---
+++ 3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf ("enter number a");
+    scanf ("%d" ,&a);
+    int b;
+    printf ("enter number b");
+    scanf ("%d",&b);
+    int c;
+    c=a+b;
+    printf (" the sum is %d",c);
+    return 0;
+}
