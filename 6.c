5ac8b17d430656ff14cb27d9429562c37603c47c
vinay <vinee9708@gmail.com>
2026-09-10 10:25:53 +0530

    program 6

---
+++ 6.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int l ,b ;
+    printf ("enter length l");
+    scanf("%d",&l);
+    printf ("enter breadth b");
+    scanf("%d",&b);
+    printf("the perimeter of rectangle is: %d", 2*(l+b));
+    return 0;
+}
