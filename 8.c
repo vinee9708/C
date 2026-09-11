10d5c2accc4d6a69f6235472e5d7bd621a9a8f56
vinay <vinee9708@gmail.com>
2026-09-11 15:36:39 +0530

    Program 8

---
+++ 8.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main (){
+    float r ;
+    printf ("enter the radius:");
+    scanf ("%f",&r);
+    printf ("The circumference of circle is : %f \n.", 2*3.14*r);
+    printf ("The area of circle is : %f \n", 3.14*r*r);
+    return 0;
+}
