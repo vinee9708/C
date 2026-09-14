08d54c42c09d07485e6f64de56590dffbd099ccf
vinay <vinee9708@gmail.com>
2026-09-14 22:29:02 +0530

    program 18

---
+++ 18.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int n;
+    printf("enter integer ");
+    scanf("%d",&n);
+    printf("N^1 %f\n",pow(n,1));
+    printf("N^2 %f\n",pow(n,2));
+    printf("N^3 %f\n",pow(n,3));
+    return 0;
+}
