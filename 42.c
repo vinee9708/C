4b0eb535cc172cc55404e91c9308513c917d24c8
vinay <vinee9708@gmail.com>
2026-09-22 23:48:13 +0530

    program 42

---
+++ 42.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int x;
+    printf("enter the number ");
+    scanf("%d",&x);
+
+    x>=1 ? printf("Natural number ") : printf("Not a natural number ");
+    return 0;
+}
