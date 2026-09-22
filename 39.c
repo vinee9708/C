e1732522a7e5f8d548cf86321a626b03b0f63c86
vinay <vinee9708@gmail.com>
2026-09-22 23:31:49 +0530

    program 39

---
+++ 39.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main (){
+    int x;
+    printf("enter the marks of student ");
+    scanf("%d",&x);
+
+    x> 30? printf("pass") : printf("fail");
+    return 0;
+    
+}
