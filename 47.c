c0eb1dfe2730f01ecee88d3fcf23ebe82d0c164b
vinay <vinee9708@gmail.com>
2026-09-24 09:05:06 +0530

    program 47

---
+++ 47.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int x;
+    printf("enter a number ");
+    scanf("%d",&x);
+
+    if(x>0){
+        printf("natural number ");
+    }
+    else{
+        printf(" not a natural number ");
+    }
+    return 0;
+}
